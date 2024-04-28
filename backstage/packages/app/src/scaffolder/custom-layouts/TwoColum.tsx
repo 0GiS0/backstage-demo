3637fbc7bbc022274f38de5cf9882b1a9d0af532
0GiS0 <giselatb@outlook.com>
2024-04-28 12:30:33 +0000

    Refactor TwoColumn layout component in TwoColum.tsx

--- backstage/packages/app/src/scaffolder/custom-layouts/TwoColum.tsx
+++ backstage/packages/app/src/scaffolder/custom-layouts/TwoColum.tsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import { scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import {
-  createScaffolderLayout,
-  LayoutTemplate,
+    createScaffolderLayout,
+    LayoutTemplate,
 } from '@backstage/plugin-scaffolder-react';
 import { Grid } from '@material-ui/core';
 
 const TwoColumn: LayoutTemplate = ({ properties, description, title }) => {
-  const mid = Math.ceil(properties.length / 2);
+    const mid = Math.ceil(properties.length / 2);
 
-  return (
-    <>
-      <h1>{title}</h1>
-      <h2>In two column layout!!</h2>
-      <Grid container justifyContent="flex-end">
-        {properties.slice(0, mid).map(prop => (
-          <Grid item xs={6} key={prop.content.key}>
-            {prop.content}
-          </Grid>
-        ))}
-        {properties.slice(mid).map(prop => (
-          <Grid item xs={6} key={prop.content.key}>
-            {prop.content}
-          </Grid>
-        ))}
-      </Grid>
-      {description}
-    </>
-  );
+    return (
+        <>
+            <h1>
+                <img src="https://www.returngis.net/wp-content/uploads/2018/12/returngis-logo.png" alt="Logo" style={{ height: '1em', marginRight: '0.5em' }} />
+                {title}
+            </h1>
+            <h2>In two column layout!!</h2>
+            <Grid container justifyContent="flex-end">
+                {properties.slice(0, mid).map(prop => (
+                    <Grid item xs={6} key={prop.content.key}>
+                        {prop.content}
+                    </Grid>
+                ))}
+                {properties.slice(mid).map(prop => (
+                    <Grid item xs={6} key={prop.content.key}>
+                        {prop.content}
+                    </Grid>
+                ))}
+            </Grid>
+            {description}
+        </>
+    );
 };
 
 export const TwoColumnLayout = scaffolderPlugin.provide(
-  createScaffolderLayout({
-    name: 'TwoColumn',
-    component: TwoColumn,
-  }),
+    createScaffolderLayout({
+        name: 'TwoColumn',
+        component: TwoColumn,
+    }),
 );
