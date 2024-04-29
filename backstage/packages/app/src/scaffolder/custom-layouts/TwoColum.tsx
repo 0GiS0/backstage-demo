de845c7b0c8f307e59f4a8f36cbf33ba08ae64bf
0GiS0 <giselatb@outlook.com>
2024-04-29 06:35:44 +0000

    Refactor TwoColumn layout component in TwoColum.tsx

--- backstage/packages/app/src/scaffolder/custom-layouts/TwoColum.tsx
+++ backstage/packages/app/src/scaffolder/custom-layouts/TwoColum.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { scaffolderPlugin } from '@backstage/plugin-scaffolder';
-import {
-    createScaffolderLayout,
-    LayoutTemplate,
-} from '@backstage/plugin-scaffolder-react';
+import { createScaffolderLayout, LayoutTemplate } from '@backstage/plugin-scaffolder-react';
 import { Grid } from '@material-ui/core';
 
 const TwoColumn: LayoutTemplate = ({ properties, description, title }) => {
@@ -12,7 +9,7 @@ const TwoColumn: LayoutTemplate = ({ properties, description, title }) => {
     return (
         <>
             <h1>
-                <img src="https://www.returngis.net/wp-content/uploads/2018/12/returngis-logo.png" alt="Logo" style={{ height: '1em', marginRight: '0.5em' }} />
+                <img src="https://www.returngis.net/wp-content/uploads/2018/12/returngis-logo.png" alt="Logo" style={{ height: '2em', marginRight: '0.5em' }} />
                 {title}
             </h1>
             <h2>In two column layout!!</h2>
