e999369bc51cbd92415b1d8a775ed5b7797ba912
0gis0 <giselatb@outlook.com>
2025-01-09 14:59:49 +0000

    Add ngrok feature to devcontainer; update API queries and server configuration

--- ghcp-extension-endpoints/index.js
+++ ghcp-extension-endpoints/index.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 app.use(morgan('combined'));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-
+const BACKSTAGE_URL = process.env.BACKSTAGE_URL || 'http://app:7007/api';
 
 /*
 * Inference Description:
@@ -18,12 +18,22 @@ Output: a JSON array with the details of the requested characters.
 */
 
 /* JSON schema
-{ 
+{
   "type": "object",
   "properties": {
-    "howManyCharacters": {
-     "type": "number",     
-      "description": "The number of Dragon Ball characters to retrieve"
+    "kind": {
+      "type": "string",
+      "default": "component",
+      "description": "The kind of entity to search for in Backstage."
+
+    },
+    "name": {
+      "type": "string",
+      "description": "The name of the entity to search for in Backstage."
+    },
+    "owner": {
+      "type": "string",
+      "description": "The owner of the entity to search for in Backstage."
     }
   }
 }
@@ -32,11 +42,37 @@ Output: a JSON array with the details of the requested characters.
 app.post('/components', async (req, res) => {
 
   console.log('Request body: ', req.body);
-  
-  let result = await fetch(`http://localhost:7007/api/catalog/entities/by-query?filter=kind=user`);
+
+  const params = {
+    kind: req.body.kind || 'component',
+    name: req.body.name,
+    owner: req.body.owner
+  };
+
+  // Construir la URL con los parámetros disponibles
+  let query = `filter=kind=${params.kind}`;
+  if (params.name) {
+    query += `&filter=metadata.name=${params.name}`;
+  }
+  if (params.owner) {
+    query += `&filter=spec.owner=${params.owner}`;
+  }
+
+  console.log('Query: ', query);
+
+  let result = await fetch(`${BACKSTAGE_URL}/catalog/entities/by-query?${query}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer hJH1SgtEaLHIMREu3W+xZqa1i/kqslzx'
+    }
+  }); 
 
   result = await result.json();
 
   res.json(result);
+});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🚀`);
 });
